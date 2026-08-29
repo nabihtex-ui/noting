c6a28fcf995202932a353369a809a69728cd9a33
nabihtex-ui <nabihtex@gmail.com>
2026-08-29 21:08:31 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ const plexArabic = IBM_Plex_Sans_Arabic({
 
 export const metadata: Metadata = {
   title: 'Nyova',
-  description: 'Nyova — download the Executor, share your feedback, and join our community on Discord.',
+  description: 'Nyova Executor — download roblox executor, share your feedback, and join our community on Discord.',
   generator: 'v0.app',
   icons: {
     icon: '/nyova-icon.png',
